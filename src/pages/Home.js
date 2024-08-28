27f8e3d1c1937f0fa0c27ac75d033bf03e63322e
Fabian <olmosfabian@hotmail.com>
2024-08-28 09:21:11 -0300

    Descripción de los cambios realizados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import '../index.css';
 const Home = () => (
   <div className="video-container">
     <video autoPlay muted loop id="background-video" className="video-fluid">
-     <source src="https://fabianolmosd.github.io/edn/video.MP4" type="video/MP4" />
-      Your browser does not support the video tag.
-     </video>
+  <source src="https://fabianolmosd.github.io/edn/video.MP4" type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
 
     <div className="overlay-text">
       <p className='p'>Bienvenido al sitio oficial de</p>
