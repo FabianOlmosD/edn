a59c0a0eea96c50c024fc14d61dac939a1516f5d
Fabian <olmosfabian@hotmail.com>
2024-08-28 09:33:34 -0300

    Descripción de los cambios realizados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../index.css';
 
-const Home = () => (
-  <div className="video-container">
-    <video autoPlay muted loop id="background-video" className="video-fluid">
-  <source src="https://fabianolmosd.github.io/edn/video.MP4" type="video/mp4" />
-  Your browser does not support the video tag.
-</video>
+const Home = () => {
+  console.log("Video source:", "https://fabianolmosd.github.io/edn/video.MP4");
 
-    <div className="overlay-text">
-      <p className='p'>Bienvenido al sitio oficial de</p>
-      <h1 className="display-1">EL DEPOSITO NORTE</h1>
+  return (
+    <div className="video-container">
+      <video autoPlay muted loop id="background-video" className="video-fluid" onError={(e) => console.error('Error loading video', e)}>
+        <source src="https://fabianolmosd.github.io/edn/video.MP4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="overlay-text">
+        <p className='p'>Bienvenido al sitio oficial de</p>
+        <h1 className="display-1">EL DEPOSITO NORTE</h1>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
+
 
 
