0afc5769dcddaffcd597c080a2775fe1461506d2
Fabian <olmosfabian@hotmail.com>
2024-08-28 08:44:15 -0300

    Updates

---
+++ precache-manifest.65fa4c973e3a5157a80de35a4c2621e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec58083009b11d26cb408dcbf59e10aa",
+    "url": "/edn/index.html"
+  },
+  {
+    "revision": "e993f529d5a16233ee8a",
+    "url": "/edn/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "ead084ddc3709e7e6126",
+    "url": "/edn/static/css/main.f3abe5d7.chunk.css"
+  },
+  {
+    "revision": "e993f529d5a16233ee8a",
+    "url": "/edn/static/js/2.a9cf0bd9.chunk.js"
+  },
+  {
+    "revision": "ead084ddc3709e7e6126",
+    "url": "/edn/static/js/main.7fd56a6f.chunk.js"
+  },
+  {
+    "revision": "496a4f1043f8ab2df413",
+    "url": "/edn/static/js/runtime~main.0e394ada.js"
+  },
+  {
+    "revision": "bf622cd154b9688a7f08549b8e910b4b",
+    "url": "/edn/static/media/BowlbyOne-Regular.bf622cd1.ttf"
+  },
+  {
+    "revision": "2d0a396df58c55f3f9426e6efc7bf9f2",
+    "url": "/edn/static/media/haypulso.2d0a396d.jpg"
+  },
+  {
+    "revision": "eafc5972ef6bd9b6bb6bd442d002a8e6",
+    "url": "/edn/static/media/pequeñas.eafc5972.jpg"
+  }
+]);
